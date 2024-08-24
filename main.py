ea0524011c1d539905994daa5b38da59b507e25d
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-08-24 17:00:22 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ os.makedirs("books", exist_ok=True)
 for id in range(10):
     id += 1
     url = f'https://tululu.org/txt.php?id={id}'
-    # print(url)
     response = requests.get(url)
     response.raise_for_status() 
     encoding = response.apparent_encoding
     filename = f'books/id{id}.txt'
     with open(filename, 'w', encoding=encoding) as f:
         f.write(response.text)
-
-# url = "https://dvmn.org/filer/canonical/1542890876/16/"
-# filename = 'dvmn.svg'
-# with open(filename, 'w') as file:
-#     file.write(response.text)
