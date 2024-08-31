6b13cfedd36c3ad2fc791cef17410fe718508a88
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-08-31 19:10:50 +0300

    add parse_book_page and cleaned up the code

--- main.py
+++ main.py
@@ -9,34 +9,29 @@ def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
 
-def get_title(id):
-    url = f'https://tululu.org/b{id}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'html.parser')
+
+def get_title(soup):
     text = soup.find('h1').text
-    book = text.split(" :: ")
-    title = book[0].replace(u'\xa0', u' ').strip()
+    main_info = text.split(" :: ")
+    title = main_info[0].replace(u'\xa0', u' ').strip()
     return title
+
 
 def download_txt(url, filename, folder="books/"):
     os.makedirs("books", exist_ok=True)
+
     response = requests.get(url)
     response.raise_for_status() 
     check_for_redirect(response)
+
     filepath = os.path.join(f'{folder}{sanitize_filename(filename)}.txt')
     with open(filepath, 'w', encoding='UTF-8') as f:
         f.write(response.text)
 
 
-def download_image(url, filename, folder="images/"):
+def download_image(filename, soup, folder="images/"):
     os.makedirs("images", exist_ok=True)
 
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
     img_url = soup.find(class_='bookimage').find('img')['src']
     if 'nopic' in img_url:
         filename = 'nopic'
@@ -52,47 +47,55 @@ def download_image(url, filename, folder="images/"):
         f.write(response.content)
 
 
-def get_comments(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    comments = soup.find_all(class_ = 'texts')
-    if comments:
-        for comment in comments:
+def parse_book_page(soup):
+    main_info_block = soup.find('h1').text
+    main_info = main_info_block.split(" :: ")
+    title = main_info[0].replace(u'\xa0', u' ').strip()
+    author = main_info[1].replace(u'\xa0', u' ').strip()
+
+    genres_block = soup.find('span', class_ = 'd_book').find_all('a')
+    correct_genres = []
+    for genre in genres_block:
+        genre_text = genre.text
+        correct_genres.append(genre_text)
+
+    comments_block = soup.find_all(class_ = 'texts')
+    correct_comments = []
+    if comments_block:
+        for comment in comments_block:
             comment = comment.find('span')
             comment_text = comment.text
-            print(comment_text)
-    else:
-        print("No comments")
+            correct_comments.append(comment_text)
 
-def get_genre(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    genres = soup.find('span', class_ = 'd_book').find_all('a')
-    # print(genres)
-    if genres:
-        for genre in genres:
-            genre_text = genre.text
-            print(genre_text)
-    else:
-        print("No genres")
+    book = {
+        'title': title,
+        'author': author,
+        'genres': correct_genres,
+        'comments': correct_comments
+    }
+
+    return book
+
 
 for id in range(10):
     try:
         id += 1
 
-        url = f'https://tululu.org/txt.php?id={id}'
-        title = get_title(id)
-        filename = f'{id}. {title}'
-        # download_txt(url, filename)
-
         url = f'https://tululu.org/b{id}/'
+        response = requests.get(url)
+        response.raise_for_status()
+        check_for_redirect(response)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        book = parse_book_page(soup)
+
         filename = f'{id}'
-        # download_image(url, filename)
-        # get_comments(url)
-        get_genre(url)
+        download_image(filename, soup)
+
+        title = get_title(soup)
+        url = f'https://tululu.org/txt.php?id={id}'
+        filename = f'{id}. {title}'
+        download_txt(url, filename)
+
+
     except requests.HTTPError:
         print('Not found book')
