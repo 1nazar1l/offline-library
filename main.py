0417b172238e79217a00169cfad799a3f120794e
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-08-31 16:41:58 +0300

    download book covers

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 def check_for_redirect(response):
@@ -28,12 +29,41 @@ def download_txt(url, filename, folder="books/"):
         f.write(response.text)
 
 
+def download_image(url, filename, folder="images/"):
+    os.makedirs("images", exist_ok=True)
+
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_url = soup.find(class_='bookimage').find('img')['src']
+    if 'nopic' in img_url:
+        filename = 'nopic'
+
+    img_url = urljoin('https://tululu.org/', img_url)
+    response = requests.get(img_url)
+    response.raise_for_status()
+    
+    file_extension = os.path.splitext(img_url)[1]
+    filepath = os.path.join(f'{folder}{filename}{file_extension}')
+
+    with open(filepath, 'wb') as f:
+        f.write(response.content)
+
+
 for id in range(10):
     try:
         id += 1
+
         url = f'https://tululu.org/txt.php?id={id}'
         title = get_title(id)
         filename = f'{id}. {title}'
         download_txt(url, filename)
+
+        url = f'https://tululu.org/b{id}/'
+        filename = f'{id}'
+        download_image(url, filename)
+        
     except requests.HTTPError:
         print('Not found book')
