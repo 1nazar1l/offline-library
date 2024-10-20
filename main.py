dd78b844223ee1129329c0caf5f8cb3dbafeb08d
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-10-20 19:08:03 +0300

    added more arguments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from urllib.parse import urljoin
 import argparse
 import time
+import json
 
 
 def check_for_redirect(response):
@@ -13,30 +14,50 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(response, filename, folder="books/"):
-    os.makedirs("books", exist_ok=True)
+def download_txt(response, filename, root_folder, folder="books/"):
+    os.makedirs(f'{root_folder}/{folder}', exist_ok=True)
 
-    filepath = os.path.join(f'{folder}{sanitize_filename(filename)}.txt')
+    filepath = os.path.join(f'{sanitize_filename(root_folder)}/{folder}{sanitize_filename(filename)}.txt')
     with open(filepath, 'w', encoding='UTF-8') as f:
         f.write(response.text)
 
 
-def download_image(filename, img_url, folder="images/"):
-    os.makedirs("images", exist_ok=True)
+def download_image(filename, img_url, root_folder, folder="images/"):
+    os.makedirs(f'{root_folder}/{folder}', exist_ok=True)
 
     if 'nopic' in img_url:
         filename = 'nopic'
 
     response = requests.get(img_url)
     response.raise_for_status()
-    # print(response)
     
     file_extension = os.path.splitext(img_url)[1]
-    # print(file_extension)
-    filepath = os.path.join(f'{folder}{filename}{file_extension}')
+    filepath = os.path.join(f'{sanitize_filename(root_folder)}/{folder}{filename}{file_extension}')
 
     with open(filepath, 'wb') as f:
         f.write(response.content)
+
+
+def download_dict(book, root_folder):
+    os.makedirs(sanitize_filename(root_folder), exist_ok=True)
+
+    filename = "books_dict.json"
+    filepath = os.path.join(f'{sanitize_filename(root_folder)}/{filename}')
+
+    if os.path.exists(filepath):
+            # Загружаем существующие данные
+            with open(filepath, 'r', encoding='UTF8') as f:
+                books = json.load(f)
+    else:
+        # Если файл не существует, создаем новый список
+        books = []
+
+    # Добавляем новый элемент в список
+    books.append(book)
+
+    # Сохраняем обновленный список обратно в файл
+    with open(filepath, 'w', encoding='UTF8') as f:
+        json.dump(books, f, ensure_ascii=False, indent=4)
 
 
 def parse_book_page(soup):
@@ -68,10 +89,15 @@ def main():
     parser = argparse.ArgumentParser(description="Скачивает книги и информацию о них")
     parser.add_argument('--start_id', type=int, default="1", help='Введите с какого id начать скачивать книги:')
     parser.add_argument('--end_id', type=int, default="10", help='Введите каким id закончить скачивание книг:')
+    parser.add_argument('--dest_folder', type=str,default="library",help='Укажите папку в которую будет сохраняться вся информация')
+    parser.add_argument('--skip_imgs', type=str,default="y",help="Укажите y/д чтобы скачивать картинки или n/н чтобы не скачивать")
+    parser.add_argument('--skip_txt', type=str,default="y",help="Укажите y/д чтобы скачивать текст или n/н чтобы не скачивать")
     args = parser.parse_args()
 
     for book_id in range(args.start_id, args.end_id):
         try:
+            root_folder = args.dest_folder
+
             url = f'https://tululu.org/b{book_id}/'
             response = requests.get(url)
             response.raise_for_status()
@@ -80,20 +106,23 @@ def main():
             book = parse_book_page(soup)
             print('Название: ', book['title'])
             print('Автор:', book['author'])
+            download_dict(book, root_folder)
 
-            img_url = book['img_url']
-            img_url = urljoin(url, img_url)
-            filename = f'{book_id}'
-            download_image(filename, img_url)
+            if args.skip_imgs == "y" or args.skip_imgs == "д":
+                img_url = book['img_url']
+                img_url = urljoin(url, img_url)
+                filename = f'{book_id}'
+                download_image(filename, img_url, root_folder)
 
-            title = book['title']
-            params = {'id': {book_id}}
-            url = 'https://tululu.org/txt.php'
-            response = requests.get(url, params=params)
-            response.raise_for_status() 
-            check_for_redirect(response)
-            filename = f'{book_id}. {title}'
-            download_txt(response, filename)
+            if args.skip_txt == "y" or args.skip_txt == "д":
+                title = book['title']
+                params = {'id': {book_id}}
+                url = 'https://tululu.org/txt.php'
+                response = requests.get(url, params=params)
+                response.raise_for_status() 
+                check_for_redirect(response)
+                filename = f'{book_id}. {title}'
+                download_txt(response, filename, root_folder)
 
             book_id += 1
 
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,83 +1,85 @@
 from bs4 import BeautifulSoup
 import requests
+
 from urllib.parse import urljoin
-from main import download_txt, download_image, parse_book_page,check_for_redirect
-import json
-import os
+from main import download_txt, download_image, parse_book_page,check_for_redirect, download_dict
 import argparse
 import sys
+import time
 
-def download_dict(book):
-    if os.path.exists('books_dict.json'):
-            # Загружаем существующие данные
-            with open('books_dict.json', 'r', encoding='UTF8') as f:
-                books = json.load(f)
+
+def main():
+    parser = argparse.ArgumentParser(description="Скачивает книги определенного жанра и информацию о них")
+    parser.add_argument('--start_page', type=int, default="1", help='Введите с какой страницы начать скачивать книги:')
+    parser.add_argument('--end_page', type=int, help='Введите какой страницей закончить скачивание книг:')
+    parser.add_argument('--dest_folder', type=str,default="library",help='Укажите папку в которую будет сохраняться вся информация')
+    parser.add_argument('--skip_imgs', type=str,default="y",help="Укажите y/д чтобы скачивать картинки или n/н чтобы не скачивать")
+    parser.add_argument('--skip_txt', type=str,default="y",help="Укажите y/д чтобы скачивать текст или n/н чтобы не скачивать")
+    args = parser.parse_args()
+
+    root_folder = args.dest_folder
+
+    if args.start_page == 1 and args.end_page is None:
+        end_page = 5
+    elif args.end_page is None:
+        end_page = args.start_page + 1
+        print("Конечное значение не указано поэтому будет скачана 1 страница")
+    elif args.start_page > args.end_page:
+        print("Начальное значение не может быть больше конечного.")
+        sys.exit(1)
     else:
-        # Если файл не существует, создаем новый список
-        books = []
+        end_page = args.end_page
 
-    # Добавляем новый элемент в список
-    books.append(book)
+    for page in range(args.start_page, end_page):
+        books_page_url = f'https://tululu.org/l55/{page}'
+        response = requests.get(books_page_url)
+        response.raise_for_status()
 
-    # Сохраняем обновленный список обратно в файл
-    with open('books_dict.json', 'w', encoding='UTF8') as f:
-        json.dump(books, f, ensure_ascii=False, indent=4)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        selector = ".d_book"
+        books = soup.select(selector)
 
+        for book in books:
+            try:
+                book_id = book.a["href"]
+                book_id = book_id.split("/")[1]
+                url = 'https://tululu.org'
+                book_url = urljoin(url,book_id)
+                print(book_url)
 
-parser = argparse.ArgumentParser(description="Скачивает книги и информацию о них")
-parser.add_argument('--start_page', type=int, default="1", help='Введите с какой страницы начать скачивать книги:')
-parser.add_argument('--end_page', type=int, help='Введите какой страницей закончить скачивание книг:')
-args = parser.parse_args()
-if args.start_page == 1 and args.end_page is None:
-    end_page = 5
-elif args.end_page is None:
-    end_page = args.start_page + 1
-    print("Конечное значение не указано поэтому будет скачана 1 страница")
-elif args.start_page > args.end_page:
-    print("Начальное значение не может быть больше конечного.")
-    sys.exit(1)
-else:
-    end_page = args.end_page
+                response = requests.get(book_url)
+                response.raise_for_status()
 
-for page in range(args.start_page, end_page):
-    books_page_url = f'https://tululu.org/l55/{page}'
-    response = requests.get(books_page_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'html.parser')
-    selector = ".d_book"
-    books = soup.select(selector)
-    for book in books:
-        try:
-            book_id = book.a["href"]
-            book_id = book_id.split("/")[1]
-            url = 'https://tululu.org'
-            book_url = urljoin(url,book_id)
-            print(book_url)
-            response = requests.get(book_url)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'html.parser')
-            book = parse_book_page(soup)
-            print('Название: ', book['title'])
-            print('Автор:', book['author'])
-            download_dict(book)
-            print("add_dict - OK")
+                soup = BeautifulSoup(response.text, 'html.parser')
+                book = parse_book_page(soup)
 
-            img_url = book['img_url']
-            img_url = urljoin(url, img_url)
-            book_id = book_id[1:]
-            filename = f'{book_id}'
-            download_image(filename, img_url)
-            print("Download img - OK")
+                print('Название: ', book['title'])
+                print('Автор:', book['author'])
+                download_dict(book, root_folder)
 
-            title = book['title']
-            params = {'id': {book_id}}
-            url = 'https://tululu.org/txt.php'
-            response = requests.get(url, params=params)
-            response.raise_for_status() 
-            check_for_redirect(response)
-            filename = f'{book_id}. {title}'
-            download_txt(response, filename)
-            print("Download text - OK")
+                if args.skip_imgs == "y" or args.skip_imgs == "д":
+                    img_url = book['img_url']
+                    img_url = urljoin(url, img_url)
+                    book_id = book_id[1:]
+                    filename = f'{book_id}'
+                    download_image(filename, img_url, root_folder)
 
-        except requests.HTTPError:
-            print('Not found book')
+                if args.skip_txt == "y" or args.skip_txt == "д":
+                    title = book['title']
+                    params = {'id': {book_id}}
+                    url = 'https://tululu.org/txt.php'
+                    response = requests.get(url, params=params)
+                    response.raise_for_status() 
+                    check_for_redirect(response)
+                    filename = f'{book_id}. {title}'
+                    download_txt(response, filename, root_folder)
+
+            except requests.HTTPError:
+                print('Not found book')
+
+            except requests.ConnectionError:
+                time.sleep(50)
+                print("Not connection, please wait")
+
+if __name__ == "__main__":
+    main()
